8386afded92b5561c1f21361813548edb9b5e091
AnnaSavina <smlap@mail.ru>
2022-01-12 21:48:52 +0300

    module - done

---
+++ modules/main.py
@@ -0,0 +1,8 @@
+from application import salary, people
+from datetime import datetime
+
+
+if __name__ == '__main__':
+    salary.calculate_salary()
+    people.get_employees()
+    print(datetime.now())
