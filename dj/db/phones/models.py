028d9299d8de44fd533790f8b25ecb6c3e76be86
Anna Savina <anna.savina@csd.ru>
2022-05-04 19:53:02 +0300

    done

--- dj/db/phones/models.py
+++ dj/db/phones/models.py
@@ -4,7 +4,6 @@ from django.db import models
 class Phone(models.Model):
     # # TODO: Добавьте требуемые поля
     # pass
-    id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=50)
     price = models.IntegerField()
     image = models.URLField()
--- dj/db/phones/views.py
+++ dj/db/phones/views.py
@@ -1,13 +1,30 @@
 from django.shortcuts import render
 
+from phones.models import Phone
+
 
 def show_catalog(request):
-    template = 'catalog.html'
+    sort = request.GET.get('sort', '').lower().strip()
     context = {}
+    if sort == 'name':
+        phones = list(Phone.objects.order_by('name').values())
+    elif sort == 'price_asc':
+        phones = list(Phone.objects.order_by('price').values())
+    elif sort == 'price_desc':
+        phones = list(Phone.objects.order_by('price').reverse().values())
+    else:
+        phones = list(Phone.objects.values())
+    template = 'catalog.html'
+    context['phones'] = phones
+    context['sort'] = sort
     return render(request, template, context)
 
 
 def show_product(request, slug):
     template = 'product.html'
     context = {}
+    phone = list(Phone.objects.filter(slug=slug).values())
+    if len(phone) > 0:
+        phone = phone[0]
+        context['phone'] = phone
     return render(request, template, context)
